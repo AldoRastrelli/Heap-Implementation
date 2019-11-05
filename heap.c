a35203714c3d595487baa691c0cd5975841d78bc
florpatrone <patroneflorenciacarla@gmail.com>
2019-11-05 16:13:11 -0300

    Creada 'heap_destruir'

--- heap.c
+++ heap.c
@@ -7,3 +7,17 @@ struct heap{
     size_t tam;
     cmp_func_t cmp;
 };
+
+/***************************
+* Primitivas del heap
+****************************/
+
+void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
+    if (destruir_elemento){
+        for (int i = 0; i < heap->cant; i++){
+            destruir_elemento(heap->datos[i]);
+        }
+    }
+    free(heap->datos);
+    free(heap);
+}
