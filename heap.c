9eac8dcc1668eef36f3ececf8f69600cd95239a4
florpatrone <patroneflorenciacarla@gmail.com>
2019-11-05 16:49:14 -0300

    Creadas 'upheap' y 'heap_encolar'

--- heap.c
+++ heap.c
@@ -1,5 +1,7 @@
 #include "heap.h"
 #include <stdlib.h>
+#define CAPACIDAD_INICIAL 20
+#define CTE_INCREMENTO 2
 
 struct heap{
     void** datos;
@@ -20,6 +22,27 @@ void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
     }
     free(heap->datos);
     free(heap);
+}
+
+void upheap(void** vector, size_t pos_elem, cmp_func_t cmp){
+    if ((pos_elem) == 0) return;
+    int pos_padre = (pos_elem-1)/2;
+    if (cmp(vector[pos_elem],vector[pos_padre]) > 0){
+        swap(vector,pos_padre,pos_elem);
+        upheap(vector,pos_padre,cmp);
+    }
+}
+
+bool heap_encolar(heap_t *heap, void *elem){
+    if (!elem) return false;
+    if (heap->cant == heap->tam){
+        if (!heap_redimensionar(heap,aumentar_capacidad)) return false;
+    }
+    void** datos = heap->datos;
+    datos[heap->cant] = elem;
+    upheap(datos,heap->cant,heap->cmp);
+    heap->cant++;
+    return true;
 }
 
 void *heap_ver_max(const heap_t *heap){
